936e2c578033ef7eef2e71f332e9e3faf3777af6
root <root@TI-84.localdomain>
2018-02-01 10:28:56 -0600

    history command working finally!

--- mysh.c
+++ mysh.c
@@ -1,7 +1,9 @@
 //Bodie Malik
 
 //TODO
-//figure out how to organize built in functions.
+//The history command needs a lot of work
+//I don't think the prof understand how bad the current framework is.
+//It can do recursive history calls. It may even get stuck in an infinite loop...
 
 
 /*
@@ -24,20 +26,31 @@ PROCESS IDEA
 
 #define MAX_TOKENS 16
 #define TEMP_CHUNK 64
+#define HISTORY_SIZE 100
 
 #define SHOW_TOKENS false
 
 //FUNCTION DECLARATIONS
 void ClearAllTokens();
 void PrintTokens();
+void SaveCommand( char *com );
 void ExecuteCommand();
+void ShowHistory();
+void ClearHistory();
+char * GetHistory(int num);
 
 
 //GLOBAL VARIABLES
 //pointer to tokens
+char *Command;
+int CommandSize = 0;
+
 char *tPoint[MAX_TOKENS];
 int tNum = 0;
 int ExitShell = 0;
+
+char *History[HISTORY_SIZE];
+int HistoryIndex = 0;
 
 
 //-------------------------MAIN FUNCTION-----------------------------
@@ -50,45 +63,59 @@ int main(){
 		printf("$");
 		
 		//Get input string
-		
-		int tempSize = TEMP_CHUNK;
-		char *tempInput = malloc(tempSize);
+		CommandSize = TEMP_CHUNK;
+		Command = malloc(CommandSize);
 		char c;
 		int i = 0;
 		while((c = getchar()) != '\n' && c != EOF)
 		{
-			tempInput[i++] = c;
+			Command[i++] = c;
 			
-			if(i == tempSize)
+			if(i == CommandSize)
 			{
-				tempSize += TEMP_CHUNK;
-				tempInput = realloc(tempInput, tempSize);
+				CommandSize += TEMP_CHUNK;
+				Command = realloc(Command, CommandSize);
 			}
 		}
-		tempInput[i] = '\0';
-		//int debugStringLength = strlen(tempInput);
-		//printf("%s=%d\n",tempInput, debugStringLength);
-		
+		Command[i] = '\0';
 		
 		//--------------------------SAVE STRING TO HISTORY--------------------------------
 		//make sure to save a copy, not just the pointer since the string will be deconstructed
+		SaveCommand(Command);
 		
-		
+		//Tokenize!!!
 		ClearAllTokens();
-		
-		//Interpret Tokens!
-		tNum = 0;
-		char *tempToken = strtok(tempInput, " ");
-		
-		while( tempToken != NULL )
+		char *tempToken = strtok(Command, " ");
+		int finished = 0;
+		do
 		{
-			if( strcmp(tempToken, "|") == 0)
+			if( tempToken == NULL)
 			{
-				//execute command and output to file
-				printf("NOT IMPLEMENTED PIPING YET! This command will be ignored.\n");
+				//execute without pipe
+				ExecuteCommand( "\0" );
+				
+				if(Command == NULL || strlen(Command) <= 0) //nothing more to execute
+				{
+					finished = 1;
+				}
+				else //last command added something to command string. Continue executing.
+				{
+					ClearAllTokens();
+					
+					tempToken = strtok(Command, " ");
+				}
+			}
+			else if( strcmp(tempToken, "|") == 0)
+			{
+				printf("NOT IMPLEMENTED PIPING YET! This command will be run without a pipe.\n");
+			
+				//execute with pipe
+				ExecuteCommand( &tempToken[2] );
 				
 				//reset tokens
 				ClearAllTokens();
+				
+				tempToken = strtok(Command, " ");
 			}
 			else
 			{
@@ -99,21 +126,17 @@ int main(){
 				}
 				else
 				{
-					printf("You have reached the max arguments for this command.\n");
-					break;
+					printf("You have reached the max arguments for this command. Argument ignored.\n");
 				}
-			}
+				
+				tempToken = strtok(NULL, " ");
 			
-			tempToken = strtok(NULL, " ");
-		}
-		
-		PrintTokens();
-		
-		//Execute command
-		ExecuteCommand();
+			}
+		}while(finished == 0);
 		
 		//clean up memory
-		free(tempInput);
+		free(Command);
+		Command = NULL;
 	}
 	
 	//Clear up memory?
@@ -125,16 +148,21 @@ int main(){
 
 
 //Executes the command currently described by the tokens.
-void ExecuteCommand()
+void ExecuteCommand(char *commandEnd)
 {
+	PrintTokens();
+	
+	int historyCopy = -1;
+	
 	if(tNum == 0) //if there are no tokens, skip
-		return;
-	//BUILT IN COMMANDS
+	{
+		printf("That command has not arguments...\n");
+	}
 	if(strcmp(tPoint[0], "exit") == 0) //EXIT
 	{
 		//BREAK FROM LOOP, ENDING SHELL
 		ExitShell = 1;
-		return;
+		//return;
 	}
 	else if(strcmp(tPoint[0], "cd") == 0) //CHANGE DIRECTORY
 	{
@@ -153,13 +181,20 @@ void ExecuteCommand()
 	{
 		if(tNum == 1)
 		{
-			printf("NOT IMPLEMENTED YET!\n");
+			ShowHistory();
 		}
 		else if(tNum == 2 && strcmp(tPoint[1], "-c") == 0)
 		{
-			printf("NOT IMPLEMENTED YET!\n");
+			ClearHistory();
 		}
-		//else if(){} //CONTINUE HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		else if(tNum == 2 && atoi(tPoint[1]) > 0)
+		{
+			historyCopy = atoi(tPoint[1]);
+		}
+		else
+		{
+			printf("That history command was not supported.\n");
+		}
 	}
 	else if(access(tPoint[0], X_OK) == 0) //OTHER EXECUTABLE
 	{
@@ -175,18 +210,41 @@ void ExecuteCommand()
 			exit(1);
 		}
 		
-		
 		//wait for child to finish
 		//printf("Parent waiting for child...\n");
 		wait(NULL);
 		//printf("\nChild finished!\n");
-		
 	}
 	else
 	{
 		//print error
 		printf("Could not find executable file %s\n", tPoint[0]);
 	}
+	
+	
+	//delete string from Command.
+	char *tempCommand = Command;
+	Command = malloc(CommandSize);
+	strcpy(Command, commandEnd);
+	free(tempCommand);
+	
+	if(historyCopy > 0)
+	{
+		
+		char *hp = GetHistory(historyCopy);
+		if(hp != NULL)
+		{
+			tempCommand = Command;
+			CommandSize += strlen(hp) + 3;
+			Command = malloc(CommandSize);
+			strcpy(Command, hp);
+			strcat(Command, tempCommand);
+			free(tempCommand);
+		}
+	}
+	
+	//debug stuff
+	//printf("NEW COMMAND: %s\n", Command);
 }
 
 
@@ -198,21 +256,11 @@ void ClearAllTokens()
 	{
 		if(tPoint[i] != NULL)
 		{
-			
-			//memmory does not need to be free since strtok does not allocate new memory.
 			tPoint[i] = NULL;
 		}
 	}
 	
 	tNum = 0;
-	
-	#if SHOW_TOKENS
-	/*
-	printf("Tokens after clear...\n");
-	PrintTokens();
-	printf("Clear Complete\n");
-	*/
-	#endif
 }
 
 
@@ -223,6 +271,7 @@ void PrintTokens()
 {
 	//Debug code to show tokens.
 	#if SHOW_TOKENS
+	printf("Printing Tokens:\n");
 	for(int i = 0; i < MAX_TOKENS; i++)
 	{
 		if(tPoint[i] != NULL)
@@ -232,4 +281,72 @@ void PrintTokens()
 		}
 	}
 	#endif
+}
+
+
+
+//----------------------------HISTORY FUNCTIOBNS-------------------------------------
+
+//This function is great because it never has to shuffle things around in the history.
+//Although, this makes viewing history and getting history elements a tad more complicated.
+void SaveCommand( char *com )
+{
+	int ti = HistoryIndex % HISTORY_SIZE;
+	if(History[ti] != NULL)
+	{
+		free(History[ti]);
+	}
+	
+	int l = strlen(com);
+	History[ti] = malloc(l);
+	strcpy(History[ti], com);
+	
+	HistoryIndex++;
+}
+
+void ShowHistory()
+{
+	int di = 0;
+	for(int i = 0; i < HISTORY_SIZE; i++)
+	{
+		int ti = (i + HistoryIndex) % HISTORY_SIZE;
+		if(History[ti] != NULL){
+			printf("%d  %s\n",di,History[ti]);
+			di++;
+		}
+	}
+}
+
+void ClearHistory()
+{
+	HistoryIndex = 0;
+	for(int i = 0; i < HISTORY_SIZE; i++)
+	{
+		if(History[i] != NULL)
+		{
+			free(History[i]);
+			History[i] = NULL;
+		}
+	}
+}
+
+char * GetHistory(int num)
+{
+	int di = 0;
+	for(int i = 0; i < HISTORY_SIZE; i++)
+	{
+		int ti = (i + HistoryIndex) % HISTORY_SIZE;
+		
+		if(History[ti] != NULL && di == num)
+		{
+			return History[ti];
+		}
+		else if(History[ti] != NULL)
+		{
+			di++;
+		}
+	}
+	
+	printf("We could not find that history command.\n");
+	return NULL;
 }
